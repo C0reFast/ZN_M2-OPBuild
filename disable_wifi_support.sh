51c749ec7e07f10fd7bf74b1eaeff3a3f0d90af0
C0reFast <chenfu@ichenfu.com>
2025-03-17 21:52:28 +0800

    切换到immortalwrt

--- disable_wifi_support.sh
+++ disable_wifi_support.sh
@@ -4,4 +4,4 @@
 sed -i '/^DEFAULT_PACKAGES/d' target/linux/qualcommax/ipq60xx/target.mk
 sed -i '/kmod-usb3 kmod-usb-dwc3 kmod-usb-dwc3-qcom/d' target/linux/qualcommax/Makefile
 sed -i 's/kmod-ath11k-ahb //' target/linux/qualcommax/Makefile
-sed -i 's/wpad-basic-mbedtls //' target/linux/qualcommax/Makefile
+sed -i 's/wpad-openssl //' target/linux/qualcommax/Makefile
